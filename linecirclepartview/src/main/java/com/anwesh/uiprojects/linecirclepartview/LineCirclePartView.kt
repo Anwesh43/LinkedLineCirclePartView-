83b6f5bade326865bf1402e5a82289080a2a0f5d
Anwesh43 <anweshthecool0@gmail.com>
2020-02-17 14:22:41 +0530

    adding LineCirclePartView class

--- linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
+++ linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawLCPNode(i : Int, scale : Float, paint : Paint) {
     drawLineCircleParts(scale, size, paint)
     restore()
 }
+
+class LineCirclePartView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
