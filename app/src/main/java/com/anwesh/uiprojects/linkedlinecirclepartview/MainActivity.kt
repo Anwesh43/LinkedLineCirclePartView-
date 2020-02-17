55f3af3a7342592d6c055deba60f04b1fefa6512
Anwesh43 <anweshthecool0@gmail.com>
2020-02-17 15:06:36 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedlinecirclepartview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinecirclepartview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedlinecirclepartview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.linecirclepartview.LineCirclePartView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineCirclePartView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
