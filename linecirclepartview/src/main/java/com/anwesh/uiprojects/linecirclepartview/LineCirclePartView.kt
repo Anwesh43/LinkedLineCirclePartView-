e6aec1ce940ae933a8cc79addcec2e8c68c89757
Anwesh43 <anweshthecool0@gmail.com>
2020-02-17 14:33:36 +0530

    adding LineCirclePart class

--- linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
+++ linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
@@ -170,4 +170,28 @@ class LineCirclePartView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineCirclePart(var i : Int) {
+
+        private val root : LCPNode = LCPNode(0)
+        private var curr : LCPNode = root
+        private var dir : Int =1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
