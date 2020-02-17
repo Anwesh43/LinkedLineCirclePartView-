dec64298f6f9b881e76dcaec0755df196b83bfad
Anwesh43 <anweshthecool0@gmail.com>
2020-02-17 14:38:48 +0530

    adding renderer in view

--- linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
+++ linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
@@ -70,14 +70,16 @@ fun Canvas.drawLCPNode(i : Int, scale : Float, paint : Paint) {
 
 class LineCirclePartView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -199,8 +201,9 @@ class LineCirclePartView(ctx : Context) : View(ctx) {
 
         private val animator : Animator = Animator(view)
         private val lcp : LineCirclePart = LineCirclePart(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             lcp.draw(canvas, paint)
             animator.animate {
