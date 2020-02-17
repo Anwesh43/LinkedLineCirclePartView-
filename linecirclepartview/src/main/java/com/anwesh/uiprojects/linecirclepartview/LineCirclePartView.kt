42cf52d28e43bcec0e1b6c7365f37b92f03a7a47
Anwesh43 <anweshthecool0@gmail.com>
2020-02-17 13:06:35 +0530

    adding constants and libraries

--- linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
+++ linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.linecirclepartview
  * Created by anweshmishra on 17/02/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.02f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
