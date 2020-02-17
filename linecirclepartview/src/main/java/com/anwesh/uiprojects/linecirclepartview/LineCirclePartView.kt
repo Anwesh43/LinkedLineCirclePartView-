6e01faf69dfde77eb7106f16e9c36071842d94ba
Anwesh43 <anweshthecool0@gmail.com>
2020-02-17 14:28:14 +0530

    adding Animator class

--- linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
+++ linecirclepartview/src/main/java/com/anwesh/uiprojects/linecirclepartview/LineCirclePartView.kt
@@ -21,6 +21,7 @@ val scGap : Float = 0.02f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rFactor : Float = 4f
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -98,6 +99,34 @@ class LineCirclePartView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
